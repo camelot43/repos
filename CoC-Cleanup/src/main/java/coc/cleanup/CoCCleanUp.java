ec2a703254f715cfe94e1c647e4930efa1d291aa
aanciaes <antonio.anciaes@gmail.com>
2017-01-09 21:55:40 +0000

    CoC Cleanup 1st commit

---
+++ CoC-Cleanup/src/main/java/coc/cleanup/CoCCleanUp.java
@@ -0,0 +1,133 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package coc.cleanup;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+/**
+ *
+ * @author miguel
+ */
+public class CoCCleanUp {
+
+    private int notValid;
+    private int valid;
+
+    private final boolean write;
+    private final String newFileName;
+    private Document newDoc;
+    private Element cocs;
+
+    public CoCCleanUp (String newFileName, boolean write) {
+        notValid=0;
+        valid=0;
+        this.write=write;
+        this.newFileName=newFileName;
+    }
+    
+    public void cleanDocument(String filename) {
+        try {
+
+            File xmlFile = new File(filename);
+
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(xmlFile);
+            doc.getDocumentElement().normalize();
+
+            if (write) {
+                newDoc = dBuilder.newDocument();
+                Element rootElement = newDoc.createElement("documents");
+                newDoc.appendChild(rootElement);
+                cocs = newDoc.createElement("cocs");
+                rootElement.appendChild(cocs);
+            }
+            NodeList nodeLst = doc.getElementsByTagName("coc");
+
+            int size = nodeLst.getLength();
+
+            for (int i = 0; i < size; i++) {
+                handleCoC(nodeLst.item(i));
+            }
+
+            System.out.println("\n-------------------------------------------\n");
+            System.out.println("Total number of nodes: " + (valid + notValid) + "\n");
+            System.out.println("Number of valid nodes: " + valid);
+            System.out.println("Number of NOT valid nodes: " + notValid);
+
+            if (write) {
+                TransformerFactory transformerFactory = TransformerFactory.newInstance();
+                Transformer transformer = transformerFactory.newTransformer();
+                newDoc.setXmlStandalone(true);
+                DOMSource source = new DOMSource(newDoc);
+                StreamResult result = new StreamResult(new File(newFileName));
+
+                transformer.transform(source, result);
+
+                System.out.println("\nNew clean xml written to: " + newFileName);
+            }
+
+            System.out.println("\n-------------------------------------------\n");
+
+        } catch (SAXException | ParserConfigurationException | TransformerException ex) {
+            Logger.getLogger(CoCCleanUp.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            System.err.println("The file: " + filename + " does not exists");
+            System.exit(0);
+        }
+    }
+
+    public void handleCoC(Node cocNode) {
+        NodeList nodeList = cocNode.getChildNodes();
+        Node cocDocument = null;
+
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            Node n = nodeList.item(i);
+            if (n.getNodeType() == Node.ELEMENT_NODE && n.getNodeName().equals("cocDocument")) {
+                cocDocument = n;
+            }
+        }
+
+        if (cocDocument != null) {
+            Element cocDocument_e = (Element) cocDocument;
+
+            if (handleCoCDocument(cocDocument_e)) {
+                if (write) {
+                    Element e = (Element) newDoc.importNode(cocNode, true);
+                    cocs.appendChild(e);
+                }
+            }
+        }
+    }
+
+    public boolean handleCoCDocument(Element cocDocumElement) {
+        if (cocDocumElement.getElementsByTagName("status").item(0).getTextContent().equals("not valid")) {
+            notValid++;
+            return false;
+        }
+        if (cocDocumElement.getElementsByTagName("status").item(0).getTextContent().equals("valid")) {
+            valid++;
+            return true;
+        }
+        return false;
+    }
+}
---
+++ CoC-Cleanup/src/main/java/coc/cleanup/Run.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package coc.cleanup;
+
+/**
+ *
+ * @author miguel
+ */
+public class Run {
+
+    public static void main(String[] args) {
+        String filename = null;
+        String newFileName = null;
+        boolean write = false;
+
+        switch (args.length) {
+            case 1:
+                filename = args[0];
+                break;
+            case 2:
+                if (args[1].equals("-w")) {
+                    filename = args[0];
+                    write = true;
+                    newFileName = "cleanXmlFile.xml";
+                    break;
+                }
+            case 3:
+                if (args[1].equals("-w")) {
+                    filename = args[0];
+                    write = true;
+                    newFileName = args[2];
+
+                    if (!newFileName.endsWith(".xml")) {
+                        newFileName = newFileName + ".xml";
+                    }
+                    break;
+                }
+            default:
+                System.err.println("Usage $java -cp CoCCleanUp.class FileName.xml [-w [newFileName].xml ]");
+                System.exit(0);
+                break;
+        }
+        
+        CoCCleanUp clean = new CoCCleanUp(newFileName, write);
+        clean.cleanDocument(filename);
+    }
+
+}
